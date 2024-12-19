6e4f1204d9386f3e8736c10988d5d45b85edf2a0
Tutu Ajayi <tutu@keplercompute.com>
2024-12-19 13:11:09 -0800

    adding support for gf12 memories

--- hard/gf_12/bsg_mem/bsg_mem_1rw_sync_macros.svh
+++ hard/gf_12/bsg_mem/bsg_mem_1rw_sync_macros.svh
@@ -3,7 +3,25 @@
 `define BSG_MEM_1RW_SYNC_MACROS_VH
 
 `define bsg_mem_1rw_sync_1rf_macro(words,bits,tag)   \
-  if (harden_p && els_p == words && width_p == bits) \
+  if (harden_p && els_p == words && width_p == 39 && "tag" == "lane") \
+    begin: macro                                     \
+      gf12_1rw_d``words``_w40_``tag``_1rf            \
+        mem                                          \
+          ( .CLK   ( clk_i  )                        \
+          , .A     ( addr_i )                        \
+          , .D     ( data_i )                        \
+          , .Q     ( data_o )                        \
+          , .CEN   ( ~v_i   )                        \
+          , .GWEN  ( ~w_i   )                        \
+          , .RET1N ( 1'b1   )                        \
+          , .STOV  ( 1'b0   )                        \
+          , .EMA   ( 3'b011 )                        \
+          , .EMAW  ( 2'b01  )                        \
+          , .EMAS  ( 1'b0   )                        \
+          );                                         \
+    end: macro                                       \
+    else                                             \
+    if (harden_p && els_p == words && width_p == bits) \
     begin: macro                                     \
       gf12_1rw_d``words``_w``bits``_``tag``_1rf      \
         mem                                          \
