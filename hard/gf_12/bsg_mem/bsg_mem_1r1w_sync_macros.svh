bb5fcc4165b9e0bd9b6a031d11aaf90433226764
Tutu Ajayi <tutu@keplercompute.com>
2025-02-18 12:56:11 -0800

    adding custom/unique memory exception

--- hard/gf_12/bsg_mem/bsg_mem_1r1w_sync_macros.svh
+++ hard/gf_12/bsg_mem/bsg_mem_1r1w_sync_macros.svh
@@ -6,7 +6,27 @@
 // Compatible with Following compilers:
 // rf_2p_hsc_rvt_mvt (Constant values are from compiler README at 0.65v)
 `define bsg_mem_1r1w_sync_2rf_macro(words,bits,tag)\
-  if (harden_p && els_p == words && width_p == bits)          \
+  if (harden_p && els_p == words && width_p == 39 && "tag" == "lane")\
+    begin: macro_z                                            \
+    gf12_1r1w_d``words``_w40_``tag``_2rf                      \
+      mem (                                                   \
+      .CLKA   (clk_i)                                         \
+      ,.CLKB  (clk_i)                                         \
+      ,.CENA  (~r_v_i)                                        \
+      ,.AA    (r_addr_i)                                      \
+      ,.QA    (r_data_o)                                      \
+      ,.CENB  (~w_v_i)                                        \
+      ,.AB    (w_addr_i)                                      \
+      ,.DB    ({1'b0,w_data_i})                               \
+      ,.EMAA  (3'b100)                                        \
+      ,.EMAB  (3'b100)                                        \
+      ,.EMASA (1'b1)                                          \
+      ,.STOV  (1'b0)                                          \
+      ,.RET1N (1'b1)                                          \
+      ,.WABL  (1'b1)                                          \
+      ,.WABLM (2'b01)                                         \
+    );                                                        \
+  end else if (harden_p && els_p == words && width_p == bits) \
     begin: macro                                              \
     gf12_1r1w_d``words``_w``bits``_``tag``_2rf                \
       mem (                                                   \
