94c70f82cb7d14d95e0d36a86248cf73c6d13076
Tutu Ajayi <tutu@keplercompute.com>
2025-02-27 11:31:00 -0800

    fixing memory macros for gf12

--- hard/gf_12/bsg_mem/bsg_mem_1rw_sync_macros.svh
+++ hard/gf_12/bsg_mem/bsg_mem_1rw_sync_macros.svh
@@ -3,27 +3,7 @@
 `define BSG_MEM_1RW_SYNC_MACROS_VH
 
 `define bsg_mem_1rw_sync_1rf_macro(words,bits,tag)   \
-  if (harden_p && els_p == words && width_p == 39 && "tag" == "lane") \
-    begin: macro                                     \
-      gf12_1rw_d``words``_w40_``tag``_1rf            \
-        mem                                          \
-          ( .CLK   ( clk_i  )                        \
-          , .A     ( addr_i )                        \
-          , .D     ( {1'b0,data_i} )                 \
-          , .Q     ( data_o )                        \
-          , .CEN   ( ~v_i   )                        \
-          , .GWEN  ( ~w_i   )                        \
-          , .RET1N ( 1'b1   )                        \
-          , .STOV  ( 1'b0   )                        \
-          , .EMA   ( 3'b101 )                        \
-          , .EMAW  ( 2'b00  )                        \
-          , .EMAS  ( 1'b0   )                        \
-          , .WABL  ( 1'b1   )                        \
-          , .WABLM ( 2'b00  )                        \
-          );                                         \
-    end: macro                                       \
-    else                                             \
-    if (harden_p && els_p == words && width_p == bits) \
+  if (harden_p && els_p == words && width_p == bits) \
     begin: macro                                     \
       gf12_1rw_d``words``_w``bits``_``tag``_1rf      \
         mem                                          \
@@ -93,6 +73,7 @@
       ,.latch_last_read_p(latch_last_read_p)                                \
       ,.num_width_bank_p(wbank)                                             \
       ,.num_depth_bank_p(dbank)                                             \
+      ,.harden_p(harden_p)                                                  \
     ) bmem (                                                                \
       .clk_i(clk_i)                                                         \
       ,.reset_i(reset_i)                                                    \
