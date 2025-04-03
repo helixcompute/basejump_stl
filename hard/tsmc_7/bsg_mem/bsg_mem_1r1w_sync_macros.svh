07bbf0bbd31707b068a379afa8ec58a615978810
Tutu Ajayi <tutu@keplercompute.com>
2025-04-03 09:14:55 -0700

    adding n7 sram support

--- hard/tsmc_7/bsg_mem/bsg_mem_1r1w_sync_macros.svh
+++ hard/tsmc_7/bsg_mem/bsg_mem_1r1w_sync_macros.svh
@@ -6,65 +6,7 @@
 // Compatible with Following compilers:
 // TODO: Re-analyze all constants
 `define bsg_mem_1r1w_sync_2rf_macro(words,bits,tag)\
-  if (harden_p && els_p == words && width_p == 39 && "tag" == "lane")\
-    begin: macro_z                                            \
-    n7_1r1w_d``words``_w40_``tag``_2rf                      \
-      mem (                                                   \
-       .AA              (r_addr_i)            \
-      ,.AB              (w_addr_i)            \
-      ,.CLKR            (clk_i)               \
-      ,.CLKW            (clk_i)               \
-      ,.D               ({1'b0,w_data_i})     \
-      ,.DFTBYP          (1'b0)                \
-      ,.DSLP            (1'b0)                \
-      ,.DSLPLV          (1'b0)                \
-      ,.FADIO           (5'b0)                \
-      ,.KP              (2'b0)                \
-      ,.PUDELAY_DSLP    ()                    \
-      ,.PUDELAY_SD      ()                    \
-      ,.Q               (r_data_o)            \
-      ,.RCT             (2'b0)                \
-      ,.REB             (~r_v_i)              \
-      ,.REDENIO         (1'b0)                \
-      ,.SD              (1'b0)                \
-      ,.SE              (1'b0)                \
-      ,.SIC             (1'b0)                \
-      ,.SID             (2'b0)                \
-      ,.SOC             ()                    \
-      ,.SOD             ()                    \
-      ,.WCT             (2'b0)                \
-      ,.WEB             (~w_v_i)              \
-    );                                                        \
-  end else if (harden_p && els_p == words && width_p == 67 && "tag" == "me")\
-    begin: macro_z2                          \
-    n7_1r1w_d``words``_w68_``tag``_2rf       \
-      mem (                                  \
-       .AA              (r_addr_i)            \
-      ,.AB              (w_addr_i)            \
-      ,.CLKR            (clk_i)               \
-      ,.CLKW            (clk_i)               \
-      ,.D               ({1'b0,w_data_i})     \
-      ,.DFTBYP          (1'b0)                \
-      ,.DSLP            (1'b0)                \
-      ,.DSLPLV          (1'b0)                \
-      ,.FADIO           (5'b0)                \
-      ,.KP              (2'b0)                \
-      ,.PUDELAY_DSLP    ()                    \
-      ,.PUDELAY_SD      ()                    \
-      ,.Q               (r_data_o)            \
-      ,.RCT             (2'b0)                \
-      ,.REB             (~r_v_i)              \
-      ,.REDENIO         (1'b0)                \
-      ,.SD              (1'b0)                \
-      ,.SE              (1'b0)                \
-      ,.SIC             (1'b0)                \
-      ,.SID             (2'b0)                \
-      ,.SOC             ()                    \
-      ,.SOD             ()                    \
-      ,.WCT             (2'b0)                \
-      ,.WEB             (~w_v_i)              \
-    );                                                        \
-  end else if (harden_p && els_p == words && width_p == bits) \
+  if (harden_p && els_p == words && width_p == bits) \
     begin: macro                                              \
     n7_1r1w_d``words``_w``bits``_``tag``_2rf                  \
       mem (                                                   \
@@ -133,20 +75,53 @@
     begin: macro                                              \
     n7_1r1w_d``words``_w``bits``_``tag``_2sram              \
       mem (                                                   \
-      .CLKA   (clk_i)                                         \
-      ,.CLKB  (clk_i)                                         \
-      ,.CENA  (~r_v_i)                                        \
-      ,.AA    (r_addr_i)                                      \
-      ,.QA    (r_data_o)                                      \
-      ,.CENB  (~w_v_i)                                        \
-      ,.AB    (w_addr_i)                                      \
-      ,.DB    (w_data_i)                                      \
-      ,.EMAA  (3'b011)                                        \
-      ,.EMAB  (3'b011)                                        \
-      ,.EMASA (1'b0)                                          \
-      ,.STOV  (1'b0)                                          \
-      ,.RET1N (1'b1)                                          \
-    );                                                        \
+ .AA           (r_addr_i) \
+,.CLKA         (clk_i) \
+,.DA           () \
+,.QA           (r_data_o) \
+,.CEBA         (~r_v_i) \
+,.WEBA         ( 1'b1) \
+,.AB           (w_addr_i) \
+,.CLKB         (clk_i) \
+,.QB           () \
+,.DB           ( w_data_i) \
+,.CEBB         ( ~w_v_i) \
+,.CEBMB        (1'b1) \
+,.WEBB         (~w_v_i) \
+,.SD           ( 'b0) \
+,.DSLP         ( 'b0) \
+,.BWEBA        ( 'b0) \
+,.BWEBB        ( 'b0) \
+,.CEBMA        ( 'b0) \
+,.WEBMA        ( 'b0) \
+,.AMA          ( 'b0) \
+,.BWEBMA       ( 'b0) \
+,.DMA          ( 'b0) \
+,.WEBMB        ( 'b0) \
+,.AMB          ( 'b0) \
+,.BWEBMB       ( 'b0) \
+,.DMB          ( 'b0) \
+,.PUDELAY_SD   () \
+,.PUDELAY_DSLP () \
+,.BIST         ( 'b0) \
+,.BISTCLKEN    ( 'b0) \
+,.CLKM         ( 'b0) \
+,.FADIO        ( 'b0) \
+,.REDENIO      ( 'b0) \
+,.SE           ( 'b0) \
+,.DFTBYP       ( 'b0) \
+,.SICA         ( 'b0) \
+,.SIDA         ( 'b0) \
+,.SOCA         ( ) \
+,.SODA         ( ) \
+,.SICB         ( 'b0) \
+,.SIDB         ( 'b0) \
+,.SOCB         ( ) \
+,.SODB         ( ) \
+,.DSLPLV       ( 'b0) \
+,.RTSEL        ( 'b0) \
+,.WTSEL        ( 'b0) \
+    );         \
   end
 
 
--- hard/tsmc_7/bsg_mem/bsg_mem_1rw_sync_macros.svh
+++ hard/tsmc_7/bsg_mem/bsg_mem_1rw_sync_macros.svh
@@ -4,34 +4,7 @@
 
 // TODO: Re-analyze all constants
 `define bsg_mem_1rw_sync_1rf_macro(words,bits,tag)   \
-  if (harden_p && els_p == words && width_p == 131 && "tag" == "lane")\
-   begin: macro_z                             \
-      n7_1rw_d``words``_w132_``tag``_1rf      \
-        mem                                   \
-          (                                   \
-            .A            (addr_i)            \
-          , .CEB          (~v_i)              \
-          , .CLK          (clk_i)             \
-          , .D            ({1'b0,data_i})     \
-          , .DFTBYP       (1'b0)              \
-          , .DSLP         (1'b0)              \
-          , .DSLPLV       (1'b0)              \
-          , .FADIO        (7'b0)              \
-          , .PUDELAY_DSLP ()                  \
-          , .PUDELAY_SD   ()                  \
-          , .Q            (data_o)            \
-          , .REDENIO      (1'b0)              \
-          , .RTSEL        (2'b0)              \
-          , .SD           (1'b0)              \
-          , .SE           (1'b0)              \
-          , .SIC          (1'b0)              \
-          , .SID          (2'b0)              \
-          , .SOC          ()                  \
-          , .SOD          ()                  \
-          , .WEB          (~w_i)              \
-          , .WTSEL        (2'b0)              \
-          );                                  \
-    end else if (harden_p && els_p == words && width_p == bits) \
+  if (harden_p && els_p == words && width_p == bits) \
     begin: macro                              \
       n7_1rw_d``words``_w``bits``_``tag``_1rf \
         mem                                   \
@@ -65,20 +38,36 @@
     begin: macro                                     \
       n7_1rw_d``words``_w``bits``_``tag``_1sram      \
         mem                                          \
-          ( .CLK   ( clk_i  )                        \
-          , .A     ( addr_i )                        \
-          , .D     ( data_i )                        \
-          , .Q     ( data_o )                        \
-          , .CEN   ( ~v_i   )                        \
-          , .GWEN  ( ~w_i   )                        \
-          , .RET1N ( 1'b1   )                        \
-          , .STOV  ( 1'b0   )                        \
-          , .EMA   ( 3'b101 )                        \
-          , .EMAW  ( 2'b00  )                        \
-          , .EMAS  ( 1'b0   )                        \
-          , .WABL  ( 1'b1   )                        \
-          , .WABLM ( 2'b00  )                        \
-          );                                         \
+          (                                   \
+  .SD('b0)  \
+  ,.DSLP('b0)  \
+  ,.CLK(clk_i)  \
+  ,.CEB(~v_i)  \
+  ,.WEB(~w_i)  \
+  ,.A(addr_i)  \
+  ,.BWEB('b0)  \
+  ,.D(data_i)  \
+  ,.Q(data_o)  \
+  ,.PUDELAY_SD()  \
+  ,.PUDELAY_DSLP()  \
+  ,.BIST('b0)  \
+  ,.CEBM('b0)  \
+  ,.WEBM('b0)  \
+  ,.AM('b0)  \
+  ,.BWEBM('b0)  \
+  ,.DM('b0)  \
+  ,.FADIO('b0)  \
+  ,.REDENIO('b0)  \
+  ,.SE('b0)  \
+  ,.DFTBYP('b0)  \
+  ,.SIC('b0)  \
+  ,.SID('b0)  \
+  ,.SOC()  \
+  ,.SOD()  \
+  ,.DSLPLV('b0)  \
+  ,.RTSEL('b0)  \
+  ,.WTSEL('b0)  \
+          );                                  \
     end: macro
 
 `define bsg_mem_1rw_sync_1hdsram_macro(words,bits,tag) \
