96de17e9b65ca2679a29b9f593e7fcea04ca2f3e
Tutu Ajayi <tutu@keplercompute.com>
2025-04-04 14:03:28 -0700

    banking wide memeory

--- hard/tsmc_7/bsg_mem/bsg_mem_1r1w_sync_macros.svh
+++ hard/tsmc_7/bsg_mem/bsg_mem_1r1w_sync_macros.svh
@@ -6,36 +6,7 @@
 // Compatible with Following compilers:
 // TODO: Re-analyze all constants
 `define bsg_mem_1r1w_sync_2rf_macro(words,bits,tag)\
- if (harden_p && els_p == words && width_p == 39 && "tag" == "lane")\
-    begin: macro_z                                            \
-    n7_1r1w_d``words``_w40_``tag``_2rf                      \
-      mem (                                                   \
-       .AA              (r_addr_i)            \
-      ,.AB              (w_addr_i)            \
-      ,.CLKR            (clk_i)               \
-      ,.CLKW            (clk_i)               \
-      ,.D               ({1'b0,w_data_i})     \
-      ,.DFTBYP          (1'b0)                \
-      ,.DSLP            (1'b0)                \
-      ,.DSLPLV          (1'b0)                \
-      ,.FADIO           (5'b0)                \
-      ,.KP              (2'b0)                \
-      ,.PUDELAY_DSLP    ()                    \
-      ,.PUDELAY_SD      ()                    \
-      ,.Q               (r_data_o)            \
-      ,.RCT             (2'b0)                \
-      ,.REB             (~r_v_i)              \
-      ,.REDENIO         (1'b0)                \
-      ,.SD              (1'b0)                \
-      ,.SE              (1'b0)                \
-      ,.SIC             (1'b0)                \
-      ,.SID             (2'b0)                \
-      ,.SOC             ()                    \
-      ,.SOD             ()                    \
-      ,.WCT             (2'b0)                \
-      ,.WEB             (~w_v_i)              \
-    );                                                        \
-  end else if (harden_p && els_p == words && width_p == 67 && "tag" == "me")\
+ if (harden_p && els_p == words && width_p == 67 && "tag" == "me")\
     begin: macro_z2                          \
     n7_1r1w_d``words``_w68_``tag``_2rf       \
       mem (                                  \
